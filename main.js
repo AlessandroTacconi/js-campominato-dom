6829d556e0e12b4fcb16927512966b91ca85c55d
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-11-16 17:34:42 +0100

    feat: bombe

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 const play = document.getElementById('play');
 const difficolta = document.getElementById('difficolta');
 const griglia = document.querySelector('.griglia');
-
+let numeriCelle;
+let numeriCasuali;
 // se clicco il bottone play genero la tabella
 // se clicco il bottone play scelgo la difficoltà
 play.addEventListener('click', function () {
@@ -22,13 +23,19 @@ play.addEventListener('click', function () {
   griglia.classList.remove('hidden');
   if (difficolta.value === 'difficolta1') {
     tabella1();
+    numeriCasuali = generaNumeriCasuali();
     console.log('difficoltà 1');
+    console.log(numeriCasuali);
   } else if (difficolta.value === 'difficolta2') {
     tabella2();
+    numeriCasuali = generaNumeriCasuali2();
     console.log('difficoltà 2');
+    console.log(numeriCasuali);
   } else if (difficolta.value === 'difficolta3') {
     tabella3();
     console.log('difficoltà 3');
+    numeriCasuali = generaNumeriCasuali3();
+    console.log(numeriCasuali);
   }
 });
 
@@ -43,7 +50,7 @@ function creazioneCelle(tag, classe, contenuto) {
 // ho creato tre funzioni diverse da mettere nell'if
 function tabella1() {
   for (let i = 1; i <= 100; i++) {
-    const numeriCelle = creazioneCelle('div', 'casella', i);
+    numeriCelle = creazioneCelle('div', 'casella', i);
     griglia.append(numeriCelle);
 
     // al click in una cella fai comparire il relativo numero in console
@@ -57,7 +64,7 @@ function tabella1() {
 
 function tabella2() {
   for (let i = 1; i <= 81; i++) {
-    const numeriCelle = creazioneCelle('div', 'casella2', i);
+    numeriCelle = creazioneCelle('div', 'casella2', i);
     griglia.append(numeriCelle);
 
     // al click in una cella fai comparire il relativo numero in console
@@ -71,7 +78,7 @@ function tabella2() {
 
 function tabella3() {
   for (let i = 1; i <= 49; i++) {
-    const numeriCelle = creazioneCelle('div', 'casella3', i);
+    numeriCelle = creazioneCelle('div', 'casella3', i);
     griglia.append(numeriCelle);
 
     // al click in una cella fai comparire il relativo numero in console
@@ -81,4 +88,64 @@ function tabella3() {
       console.log(i);
     });
   }
+}
+
+/*
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+*/
+
+// creo tre diversi generatori di numeri per generare 16 numeri casuali da 0 a 100, da 0 a 81 e da 0 a 49
+function generaNumeriCasuali() {
+  let numeriGenerati = {};
+  let arrayNumeri = [];
+
+  while (arrayNumeri.length < 16) {
+    let numeroCasuale = Math.floor(Math.random() * 100) + 1;
+
+    if (!numeriGenerati[numeroCasuale]) {
+      numeriGenerati[numeroCasuale] = true;
+      arrayNumeri.push(numeroCasuale);
+    }
+  }
+
+  return arrayNumeri;
+}
+
+//
+
+function generaNumeriCasuali2() {
+  let numeriGenerati = {};
+  let arrayNumeri = [];
+
+  while (arrayNumeri.length < 16) {
+    let numeroCasuale = Math.floor(Math.random() * 81) + 1;
+
+    if (!numeriGenerati[numeroCasuale]) {
+      numeriGenerati[numeroCasuale] = true;
+      arrayNumeri.push(numeroCasuale);
+    }
+  }
+
+  return arrayNumeri;
+}
+
+//
+
+function generaNumeriCasuali3() {
+  let numeriGenerati = {};
+  let arrayNumeri = [];
+
+  while (arrayNumeri.length < 16) {
+    let numeroCasuale = Math.floor(Math.random() * 49) + 1;
+
+    if (!numeriGenerati[numeroCasuale]) {
+      numeriGenerati[numeroCasuale] = true;
+      arrayNumeri.push(numeroCasuale);
+    }
+  }
+
+  return arrayNumeri;
 }
